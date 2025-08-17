1d04f37c77d92964bb509e9d2bc067be4dcbb8bc
zbb88888 <jmdxjsjgcxy@gmail.com>
2025-08-17 23:31:26 +0800

    Add clean script and update release script
    Added clean.sh to remove build artifacts and .spec files. Updated release.sh to use pyinstaller with improved options and added explanatory comments.

--- release.sh
+++ release.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-pyinstaller -D -w -i "icon.ico" freeduck.py
+# pyinstaller -D -w -i "icon.ico" main.py
 
-# clean
-# rm -fr dist/ build/ *.spec
+pyinstaller --onefile --windowed --name "FreeDuck" --icon "icon.ico" main.py
+
+# --onefile: Bundles everything into a single executable file (e.g., your_script_name.exe on Windows). If omitted, PyInstaller creates a directory containing the executable and its dependencies.
+# --name "YourAppName": Specifies the name for the output executable and the generated .spec file.
+# --windowed or --noconsole: Creates a GUI application without a console window. This is useful for applications with a graphical user interface.
+# --add-data "source_path;destination_path": Includes additional data files (e.g., images, configuration files) in the bundle. The destination_path is relative to the root of the bundled application.
+# --icon "icon.ico": Specifies a custom icon for the executable.
